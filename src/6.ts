257bf8856d92c2d04f91127958b527220830d006
andrei9669 <ansiro@ttu.ee>
2021-12-07 08:02:42 +0200

    init

---
+++ src/6.ts
@@ -0,0 +1,47 @@
+const inputT = `3,3,5,1,1,3,4,2,3,4,3,1,1,3,3,1,5,4,4,1,4,1,1,1,3,3,2,3,3,4,2,5,1,4,1,2,2,4,2,5,1,2,2,1,1,1,1,4,5,4,3,1,4,4,4,5,1,1,4,3,4,2,1,1,1,1,5,2,1,4,2,4,2,5,5,5,3,3,5,4,5,1,1,5,5,5,2,1,3,1,1,2,2,2,2,1,1,2,1,5,1,2,1,2,5,5,2,1,1,4,2,1,4,2,1,1,1,4,2,5,1,5,1,1,3,1,4,3,1,3,2,1,3,1,4,1,2,1,5,1,2,1,4,4,1,3,1,1,1,1,1,5,2,1,5,5,5,3,3,1,2,4,3,2,2,2,2,2,4,3,4,4,4,1,2,2,3,1,1,4,1,1,1,2,1,4,2,1,2,1,1,2,1,5,1,1,3,1,4,3,2,1,1,1,5,4,1,2,5,2,2,1,1,1,1,2,3,3,2,5,1,2,1,2,3,4,3,2,1,1,2,4,3,3,1,1,2,5,1,3,3,4,2,3,1,2,1,4,3,2,2,1,1,2,1,4,2,4,1,4,1,4,4,1,4,4,5,4,1,1,1,3,1,1,1,4,3,5,1,1,1,3,4,1,1,4,3,1,4,1,1,5,1,2,2,5,5,2,1,5`;
+const input = `3,4,3,1,2`;
+
+const fishes = input.split(',').map(Number);
+let arr: { total: number; counter: number }[] = fishes.map((num) => ({
+  total: 1,
+  counter: num,
+}));
+
+for (let i = 1; i <= 256; i++) {
+  arr = arr.map((obj) => {
+    obj.counter -= 1;
+
+    return obj;
+  });
+
+  const expired = arr.filter((obj) => obj.counter < 0);
+  arr = arr.filter((obj) => obj.counter >= 0);
+
+  if (expired.length > 0) {
+    const index = arr.findIndex((obj) => obj.counter === 6);
+
+    expired.forEach((obj) => {
+      if (index > -1) {
+        arr[index].total += obj.total;
+      } else {
+        arr.push({
+          counter: 6,
+          total: obj.total,
+        });
+      }
+
+      arr.push({
+        counter: 8,
+        total: obj.total,
+      });
+    });
+  }
+}
+
+let total = 0;
+arr.forEach((obj) => {
+  total += obj.total;
+});
+console.log(arr.length)
+console.log(total);
+export {};
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
 // import './1';
+// import './2';
+// import './3';
+// import './4';
+// import './5';
+// import './6';
+import './7';
